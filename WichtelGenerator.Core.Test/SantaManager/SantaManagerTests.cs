d9d3b514d13ffdcd5209187ae20b8b4b9b87bccd
Tim Rutschi <timrutschi@gmx.ch>
2021-08-02 23:50:04 +0200

    Add Skeleton for SantaManagerTests

---
+++ WichtelGenerator.Core.Test/SantaManager/SantaManagerTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+
+namespace WichtelGenerator.Core.Test.SantaManager
+{
+    public class SantaManagerTests
+    {
+        //Given-When-Then
+        [Test]
+        public void SantaHasNotBeenAddedYet_SantaAdded_EventShouldInvoke()
+        {
+            
+        }
+
+        [Test]
+        public void SantaHasNotBeenAddedYet_SantaAdded_SantaSouldBeInTheList()
+        {
+            
+        }
+
+        [Test]
+        public void SantaIsAlreadyAdded_SantaAdded_ReturnsAlreadyExistsResult()
+        {
+            
+        }
+        
+        [Test]
+        public void SantaHasNotBeenAddedYet_SantaAddedWithExitsName_ReturnsAlreadyUsedResult()
+        {
+            
+        }
+        
+        [Test]
+        public void SantaHasNotBeenAddedYet_SantaAdded_SantaShouldBeOnOthersWhiteList()
+        {
+            
+        }
+        
+        [Test]
+        public void SantaHasNotBeenAddedYet_SantaAdded_SantaShouldBeOnHisOwnBlackList()
+        {
+            
+        }
+        
+        [Test]
+        public void MultibleSantasAdded_SantaMovedToBlackList_SantaShouldBeRemovedFromWhiteList()
+        {
+            
+        }
+        
+        [Test]
+        public void MultibleSantasAdded_SantaMovedToBlackList_SantaShouldHasNotZeroWhiteListSantas()
+        {
+            
+        }
+        
+        [Test]
+        public void MultibleSantasAdded_SantaMovedToBlackList_NotMoreThanOneShouldHasOnlyTheSameSantaOnWhiteList()
+        {
+            
+        }
+        
+        [Test]
+        public void MultibleSantasAdded_SantaMovedToWhiteList_SantaShouldBeRemovedOnBlackList()
+        {
+            
+        }
+        
+        [Test]
+        public void MultibleSantasAdded_SantaRemovedFromMember_SantaShouldBeRomovedOnEveryList()
+        {
+            
+        }
+    }
+}
